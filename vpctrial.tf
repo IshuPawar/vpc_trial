e002218bffda3ac9feaf26f45807fcac65376779
IshuPawar <134053759+IshuPawar@users.noreply.github.com>
2023-05-22 17:48:02 +0530

    Update vpctrial.tf

--- vpctrial.tf
+++ vpctrial.tf
@@ -36,7 +36,7 @@ resource "aws_internet_gateway" "igw" {
 }
 resource "aws_route_table" "rt" {
     vpc_id = aws_vpc.myvpc.id
-    route = {
+    route  {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.igw.id
     }
